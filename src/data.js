47a83ef2b4eafe5331204fe1fb5ea9e84215b475
MONIKA SHARMA <monika2004sharma@gmail.com>
2021-04-08 01:42:49 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+exports.products = [
+{
+    id: 1,
+    Name: "Javascript for Begineers",
+    price: "2000",
+    Currency: "$",
+    Description: "Discounted price"
+},
+{
+    id: 2,
+    Name: "HTML for Begineers",
+    price: "2000",
+    Currency: "$",
+    Description: "Discounted price"
+},
+{
+    id: 3,
+    Name: "CSS for Begineers",
+    price: "2000",
+    Currency: "$",
+    Description: "Discounted price"
+},
+{
+    id: 4,
+    Name: "React JS for Begineers",
+    price: "2000",
+    Currency: "$",
+    Description: "Discounted price"
+}
+]
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const { products} = require("./data");
+
+
+
+const PORT = process.env.PORT || 3000 ;
+
+const key_id = "rzp_test_MdGluNtTWCvuX9";
+const key_secret = "6KXQDPcq6R77mzBbjVmbOqHr";
+var instance = new Razorpay({
+  key_id,
+  key_secret
+})
+
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+
+app.get("/products", (req,res) => {
+ res.status(200).json(products)
+});
+
+app.get("/order/:productID", (req,res) => {
+  const { productID } = req.params;
+  const product = products.find((product) => product.id = productID)
+ const amount = product.price * 100 * 70;
+ const currency = product.Currency;
+ const receipt = 'receipt#123';
+ const notes = { desc: product.Description };
+instance.orders.create({amount, currency, receipt, notes}, (error, order) => {
+  if (error) {
+    return res.status(500).json(error);
+  }
+  return res.status(200).json(order);
+})
+});
+
+app.post("/verify/razorpay-signature", (req,res) => {
+  console.log(JSON.stringify(req.body));
+const crypto = require('crypto');
+const sha256 = crypto.createHash('SHA256', "1234567@20").update(JSON.stringify(req.body)).digest("hex");
+console.log(hash)
+console.log(req.headers["x-razorpay-signature"])
+if(hash===req.headers["x-razorpay-signature"]){
+  console.log("payment success")
+}else{
+  console.log("payment declined");
+}
+  res.status(200).json(products)
+ });
+
+app.listen(PORT, () => {
+  console.log(`server started on port : ${PORT}`);
+});
